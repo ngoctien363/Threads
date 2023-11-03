fac11a88b333e990f872b64c9dd417b0edf843ec
ngoctien363 <dangngoctien98@gmail.com>
2023-11-03 22:09:03 +0700

    done activity and communitites page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from 'next/font/google';
 import '../globals.css';
+import { dark } from "@clerk/themes";
 
 export const metadata = {
     title: 'Threads',
@@ -11,10 +12,14 @@ const inter = Inter({subsets: ["latin"]})
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
-        <ClerkProvider>
+        <ClerkProvider appearance={{
+            baseTheme: dark,
+          }}>
             <html lang="en">
                 <body className={`${inter.className} bg-dark-1`}>
-                    {children}
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        {children}
+                    </div>
                 </body>
             </html>
         </ClerkProvider>
