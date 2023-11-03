db29b926bac78e17ac4ac0f4c3077d645a9e3fe8
ngoctien363 <dangngoctien98@gmail.com>
2023-11-03 21:06:12 +0700

    done profile page

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 import { sidebarLinks } from './../../constants/index';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
-import { SignOutButton, SignedIn } from '@clerk/nextjs';
+import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
 function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
                 {sidebarLinks.map((link) => {
                     const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+                    if (link.route === "/profile") link.route = `${link.route}/${userId}`;
                     return(
                     <div>
                         <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
+import Community from "../models/community.model";
 
 interface Params {
     userId: string,
@@ -34,6 +36,38 @@ export async function updateUser({userId, bio, name, path, username, image}: Par
         
     }
 }
+
+export async function fetchUserPosts(userId: string) {
+    try {
+      connectToDB();
+  
+      // Find all threads authored by the user with the given userId
+      const threads = await User.findOne({ id: userId }).populate({
+        path: "threads",
+        model: Thread,
+        populate: [
+          {
+            path: "community",
+            model: Community,
+            select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "name image id", // Select the "name" and "_id" fields from the "User" model
+            },
+          },
+        ],
+      });
+      return threads;
+    } catch (error) {
+      console.error("Error fetching user threads:", error);
+      throw error;
+    }
+  }
 
 export async function fetchUser(userId: string) {
     try {
